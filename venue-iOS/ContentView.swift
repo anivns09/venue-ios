8c5821943fa526ed2e93308849f1150a32b455e4
Anirudh <anivns09@gmail.com>
2026-04-15 06:44:37 +1000

    Initial Setup: Create initial iOS project

---
+++ venue-iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  venue-iOS
+//
+//  Created by Anirudh Pandey on 15/4/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ venue-iOS/venue_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  venue_iOSApp.swift
+//  venue-iOS
+//
+//  Created by Anirudh Pandey on 15/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct venue_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
